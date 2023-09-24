4125f22aa03aed6721422397cafeebe86c3002f6
Stéphane <von.zaryavtre@gmail.com>
2023-09-24 03:48:06 +0100

    log 002

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ function getRandomHexColors() {
   return hexa
 }
 
-console.log(getRandomHexColors())
-
 fetch(api)
   .then((res) => res.json())
   .then((data) => {
+    console.log(data)
     document.querySelector('#first').style.backgroundColor = data.hex.value
     document.querySelector('#second').style.backgroundColor = data.hex.value
     document.querySelector('#third').style.backgroundColor = data.hex.value
     document.querySelector('#fourth').style.backgroundColor = data.hex.value
     document.querySelector('#fifth').style.backgroundColor = data.hex.value
+
+    const getHexHtml = document.querySelectorAll('.hex')
+    for (let i = 0; i < getHexHtml.length; i++) {
+      getHexHtml[i].textContent = data.hex.value
+    }
   })
